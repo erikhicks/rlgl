a99e46ab8a3ad89c8031142d0aae01871174cf14
RLGL pi <pi@raspberrypi.(none)>
2013-10-04 00:34:45 +0000

    fixed tabbed spacing in changecolor

--- changecolor.py
+++ changecolor.py
@@ -15,25 +15,25 @@ def all_off():
   red.turn_off()
   green.turn_off()
   yellow.turn_off()
-	purple.turn_off()
+  purple.turn_off()
 
 if (len(sys.argv) == 2):
 
   if (sys.argv[1] == 'red'):
     red.turn_on()
-		purple.turn_off()
+    purple.turn_off()
     green.turn_off()
  
   if (sys.argv[1] == 'green'):
     red.turn_off()
-		purple.turn_off()
+    purple.turn_off()
     green.turn_on()
 		
 
-	if (sys.argv[1] == 'purple'):
+  if (sys.argv[1] == 'purple'):
     red.turn_off()
     green.turn_off()
-		purple.turn_on()
+    purple.turn_on()
  
   if (sys.argv[1] == 'init'):
     piface.pfio.init()
@@ -42,8 +42,8 @@ if (len(sys.argv) == 2):
     red.turn_on()
     time.sleep(1)
     yellow.turn_on()
-		time.sleep(1)
-		purple.turn_on()
+    time.sleep(1)
+    purple.turn_on()
     time.sleep(1)
     all_off()
 
@@ -60,7 +60,7 @@ if (len(sys.argv) == 3):
     if (sys.argv[1] == 'green'):
       green.turn_on()
 
-  	if (sys.argv[1] == 'purple'):
+    if (sys.argv[1] == 'purple'):
       green.turn_on()
  
     if (sys.argv[1] == 'yellow'):
@@ -77,7 +77,7 @@ if (len(sys.argv) == 3):
     if (sys.argv[1] == 'yellow'):
       yellow.turn_off()
 
-		if (sys.argv[1] == 'purple'):
+    if (sys.argv[1] == 'purple'):
       purple.turn_on()
 
 
